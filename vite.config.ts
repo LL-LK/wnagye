2539fef1e5d1553429585009cbeb0ee11c970cae
LL-LK <2214097561@qq.com>
2026-04-27 00:52:02 +0800

    修复 Vite 配置：将 'url' 改为 'node:url' 以解决模块找不到问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'node:url';
 export default defineConfig({
   plugins: [vue()],
   resolve: {
