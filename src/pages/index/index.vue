66a2d9a19f14b56ed5ba5d154a30e303656ccede
LL-LK <2214097561@qq.com>
2026-04-26 21:57:52 +0800

    修复背景图居中显示，消除右侧留白

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -674,13 +674,14 @@ const closeZoom = () => {
 
 .banner-bg {
   position: absolute;
-  top: 0;
-  left: 0;
+  top: 50%;
+  left: 50%;
   width: 100%;
   height: 100%;
   z-index: 1;
   filter: brightness(0.7);
   object-fit: cover;
+  transform: translate(-50%, -50%);
 }
 
 .banner-content {
